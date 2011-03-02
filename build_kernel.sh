9739fd6e3e40b18beeb79a64a6cbcb0191ec922a
nullghost <nullghost.android@gmail.com>
2011-03-01 22:21:59 -0600

    update build script to suck less.

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,124 +1,90 @@
-#/bin/bash
+#!/bin/sh
 
-echo "$1 $2 $3"
+# Defaults
+BUILD_KERNEL=y
+CLEAN=n
+CROSS_COMPILE="$PWD/../arm-2010q1/bin/arm-none-eabi-"
+DEFCONFIG=n
+MKZIP='7z -mx9 -mmt=1 a "$OUTFILE" .'
+PRODUCE_TAR=n
+PRODUCE_ZIP=n
+TARGET="victory_03"
+THREADS=2
+VERSION=$(date +%m%d%Y)
 
-case "$1" in
-	Clean)
-		echo "********************************************************************************"
-		echo "* Clean Kernel                                                                 *"
-		echo "********************************************************************************"
-
-		pushd Kernel
-		make clean
-		popd
-		echo " It's done... "
-		exit
-		;;
-	*)
-		PROJECT_NAME=SPH-D700
-		HW_BOARD_REV="03"
-		;;
-esac
-
-if [ "$CPU_JOB_NUM" = "" ] ; then
-	CPU_JOB_NUM=8
-fi
-
-TARGET_LOCALE="vzw"
-
-TOOLCHAIN=`pwd`/../arm-2009q3/bin
-TOOLCHAIN_PREFIX=arm-none-eabi-
-
-KERNEL_BUILD_DIR=`pwd`/Kernel
-ANDROID_OUT_DIR=`pwd`/Android/out/target/product/SPH-D700
-
-export PRJROOT=$PWD
-export PROJECT_NAME
-export HW_BOARD_REV
-
-export LD_LIBRARY_PATH=.:${TOOLCHAIN}/../lib
-
-echo "************************************************************"
-echo "* EXPORT VARIABLE		                            	 *"
-echo "************************************************************"
-echo "PRJROOT=$PRJROOT"
-echo "PROJECT_NAME=$PROJECT_NAME"
-echo "HW_BOARD_REV=$HW_BOARD_REV"
-echo "************************************************************"
-
-BUILD_MODULE()
-{
-	echo "************************************************************"
-	echo "* BUILD_MODULE	                                       	 *"
-	echo "************************************************************"
-	echo
-
-	pushd Kernel
-		make ARCH=arm modules
-	popd
-}
-
-BUILD_KERNEL()
-{
-	echo "************************************************************"
-	echo "*        BUILD_KERNEL                                      *"
-	echo "************************************************************"
-	echo
-
-
-	pushd $KERNEL_BUILD_DIR
-
-	export KDIR=`pwd`
-
-	make ARCH=arm victory_03_defconfig
-
-	# make kernel
-
-	make -j$CPU_JOB_NUM ARCH=arm CROSS_COMPILE=$TOOLCHAIN/$TOOLCHAIN_PREFIX
-
-
-	popd
-	
-}
-
-# print title
-PRINT_USAGE()
-{
-	echo "************************************************************"
-	echo "* PLEASE TRY AGAIN                                         *"
-	echo "************************************************************"
-	echo
-}
-
-PRINT_TITLE()
+SHOW_HELP()
 {
 	echo
-	echo "************************************************************"
-	echo "*                     MAKE PACKAGES"
-	echo "************************************************************"
-	echo "* 1. kernel : zImage"
-	echo "* 2. modules"
-	echo "************************************************************"
-}
-
-##############################################################
-#                   MAIN FUNCTION                            #
-##############################################################
-if [ $# -gt 3 ]
-then
+	echo "Usage options for build_kernel.sh:"
+	echo "-c : Run 'make clean'"
+	echo "-d : Use specified config."
+	echo "     For example, use -d myconfig to 'make myconfig_defconfig'"
+	echo "-h : Print this help."
+	echo "-j : Use a specified number of threads to build."
+	echo "     For example, use -j4 to make with 4 threads."
+	echo "-t : Produce tar file suitable for flashing with Odin."
+	echo "-z : Produce zip file suitable for flashing via Recovery."
 	echo
-	echo "**************************************************************"
-	echo "*  Option Error                                              *"
-	PRINT_USAGE
 	exit 1
+}
+
+# Get values from Args
+set -- $(getopt cd:hj:tz "$@")
+while [ $# -gt 0 ]
+do
+	case "$1" in
+	(-c) CLEAN=y;;
+	(-d) DEFCONFIG=y; TARGET="$2"; shift;;
+	(-h) SHOW_HELP;;
+	(-j) THREADS=$2; shift;;
+	(-t) PRODUCE_TAR=y;;
+	(-z) PRODUCE_ZIP=y;;
+	(--) shift; break;;
+	(-*) echo "$0: error - unrecognized option $1" 1>&2; exit 1;;
+	(*) break;;
+	esac
+	shift
+done
+
+echo "make clean    == "$CLEAN
+echo "use defconfig == "$DEFCONFIG
+echo "build target  == "$TARGET
+echo "make threads  == "$THREADS
+echo "build kernel  == "$BUILD_KERNEL
+echo "create tar    == "$PRODUCE_TAR
+echo "create zip    == "$PRODUCE_ZIP
+
+if [ "$CLEAN" = "y" ] ; then
+	echo "Cleaning source directory." && echo ""
+	make -j"$THREADS" ARCH=arm clean
 fi
 
-START_TIME=`date +%s`
+if [ "$DEFCONFIG" = "y" -o ! -f ".config" ] ; then
+	echo "Using default configuration for $TARGET" && echo ""
+	make -j"$THREADS" ARCH=arm ${TARGET}_defconfig
+fi
 
-PRINT_TITLE
+if [ "$BUILD_KERNEL" = "y" ] ; then
+	T1=$(date +%s)
+	echo "Beginning zImage compilation..." && echo ""
+	make -j"$THREADS" ARCH=arm CROSS_COMPILE="$CROSS_COMPILE"
+	T2=$(date +%s)
+	echo "" && echo "Compilation took $(($T2 - $T1)) seconds." && echo ""
+fi
 
-BUILD_KERNEL
-END_TIME=`date +%s`
-let "ELAPSED_TIME=$END_TIME-$START_TIME"
-echo "Total compile time is $ELAPSED_TIME seconds"
+if [ "$PRODUCE_TAR" = y ] ; then
+	echo "Generating $TARGET-$VERSION.tar for flashing with Odin" && echo ""
+	tar c -C arch/arm/boot zImage >"$TARGET-$VERSION.tar"
+fi
 
+if [ "$PRODUCE_ZIP" = y ] ; then
+	echo "Generating $TARGET-$VERSION.zip for flashing as update.zip" && echo ""
+	rm -fr "$TARGET-$VERSION.zip"
+	rm -f update/kernel_update/zImage
+	cp arch/arm/boot/zImage update/kernel_update
+	OUTFILE="$PWD/$TARGET-$VERSION.zip"
+	cd update
+	eval "$MKZIP" >/dev/null 2>&1
+	cd ..
+	mv $"OUTFILE"-signed "$OUTFILE"
+fi
